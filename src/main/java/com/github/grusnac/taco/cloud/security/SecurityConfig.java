4bbb351154fcf762938b317ba8441832f3a8357d
Grigore Rusnac <en.grigore.rusnac@gmail.com>
2021-03-19 20:29:48 +0200

    CH4 LADP embedet server authentication

--- src/main/java/com/github/grusnac/taco/cloud/security/SecurityConfig.java
+++ src/main/java/com/github/grusnac/taco/cloud/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.grusnac.taco.cloud.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,14 +11,42 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("buzz")
-                .password("{noop}infinity")
-                .authorities("ROLE_USER")
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .fullyAuthenticated()
                 .and()
-                .withUser("woody")
-                .password("{noop}bullseye")
-                .authorities("ROLE_USER");
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authentication) throws Exception {
+//        authentication.inMemoryAuthentication()
+//                .withUser("buzz")
+//                .password("{noop}infinity")
+//                .authorities("ROLE_USER")
+//                .and()
+//                .withUser("woody")
+//                .password("{noop}bullseye")
+//                .authorities("ROLE_USER");
+
+//        JDBC Authentication
+//        authentication.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery("select username, password, enabled from Users where username = ?")
+//                .authoritiesByUsernameQuery("select username, authority from UserAuthorities where username = ?")
+//                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+
+//        LDAP Authentication
+        authentication
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=tacocloud,dc=org")
+                .and()
+                .passwordCompare()
+//                .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
 }
